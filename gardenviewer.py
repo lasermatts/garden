9fe651f13ea9e0c6ba1922cf82f2e99781645126
Matt <mattpuchalski5@gmail.com>
2022-12-08 17:40:16 +0000

    tracking garden projects

---
+++ gardenviewer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+##let's plot everything fun! 
+
+##IMPORTS
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+##load the dataset
+df = pd.read_csv('~/programming/data_analysis/pittsburgh_gardening/garden_dec_2017.csv')
+
+##cool we're moving, let's try to view it
+df.head()
+
+##END
